5b732f5c428c6a737cfda8353ee86a4f36d05ba5
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-15 15:32:41 +0530

    some bug fixes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+
 const express = require("express");
 const app = express();
 const https = require("node:https");
@@ -6,8 +6,8 @@ const https = require("node:https");
 app.get("/", function (req, res) {
 
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric";
-    https.get( url , function (response){
-        console.log(response);
+    https.get( url ,  (response) =>{
+        //console.log(response);
     }); 
     
   response.send("success");
