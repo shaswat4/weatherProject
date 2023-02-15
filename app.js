df0592ad2d001df340069da71a2211fb221e34b9
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-15 18:25:36 +0530

    added body parser and removed all the api code from get request, instead sending index.html

--- app.js
+++ app.js
@@ -2,40 +2,50 @@
 const express = require("express");
 const app = express();
 const https = require("node:https");
+const bodyParser = require("body-parser");
+
+app.use( bodyParser.urlencoded({extended : true }));
 
 app.get("/", function (req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric";
-    https.get( url ,  (response) =>{
-        //console.log(response);
-
-        response.on("data" , function(data){
-            //console.log(data);
-            const weatherData = JSON.parse( data);
-            //console.log(weatherData);
-            const description =  weatherData.weather[0].description;
-            const temp = weatherData.main.temp ;
-            const code =  weatherData.weather[0].icon ;
-            const id = weatherData.weather[0].id ;
-
-
-            console.log(description);
-            console.log(temp);
-            console.log(code);
-            console.log(id);
-            
-            res.write("<p>the weather is currently " + description + "</p>");
-            res.write("<h2>the temprature in Delhi is currenly " + temp +" degrees celcius</h2>");
-            res.write( "<img src='http://openweathermap.org/img/wn/" +code+ "@2x.png' >");
-
-            res.send();
-
-
-        });
-    }); 
+    res.sendFile(__dirname + "/index.html");
     
-  //res.send("success");
 });
+
+app.post( "/" , function (req , res){
+
+
+} );
+
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric";
+//     https.get( url ,  (response) =>{
+//         //console.log(response);
+
+//         response.on("data" , function(data){
+//             //console.log(data);
+//             const weatherData = JSON.parse( data);
+//             //console.log(weatherData);
+//             const description =  weatherData.weather[0].description;
+//             const temp = weatherData.main.temp ;
+//             const code =  weatherData.weather[0].icon ;
+//             const id = weatherData.weather[0].id ;
+
+
+//             // console.log(description);
+//             // console.log(temp);
+//             // console.log(code);
+//             // console.log(id);
+            
+//             res.write("<p>the weather is currently " + description + "</p>");
+//             res.write("<h2>the temprature in Delhi is currenly " + temp +" degrees celcius</h2>");
+//             res.write( "<img src='http://openweathermap.org/img/wn/" +code+ "@2x.png' >");
+
+//             res.send();
+
+
+//         });
+//     }); 
+    
 
 app.listen(3000, () => {
   console.log("server up at port 3000");
