ac46539dfd55594cad1b77760578c226f43610cd
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-15 15:27:02 +0530

    added ;

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const https = require("node:https");
 
 app.get("/", function (req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric";
     https.get( url , function (response){
         console.log(response);
     }); 
