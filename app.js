121535f1317516269880fbed1e30b76d7a781343
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-15 18:01:20 +0530

    parsed get data with json and console logged it

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ app.get("/", function (req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=46ea98a5684d2fa1cc9b785e4a32ff4f&units=metric";
     https.get( url ,  (response) =>{
         //console.log(response);
+
+        response.on("data" , function(data){
+            //console.log(data);
+            const weatherData = JSON.parse( data);
+            //console.log(weatherData);
+            const description =  weatherData.weather[0].description;
+            const temp = weatherData.main.temp ;
+
+
+            console.log(description);
+            console.log(temp);
+
+
+        });
     }); 
     
-  response.send("success");
+  res.send("success");
 });
 
 app.listen(3000, () => {
